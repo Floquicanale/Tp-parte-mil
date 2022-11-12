149f25186392bd6c97e6f1232e198afe3a291f8c
floquicanale <fcanale@itba.edu.ar>
2022-11-12 18:55:58 -0300

    Scripts & stuff

--- Assets/remote.cs
+++ Assets/remote.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class remote : MonoBehaviour {
     private Canvas CanvasObject; // Assign in inspector
- 
+
+    [SerializeField]
+    private AudioSource Audio;
+    bool audioPlay = false;
+
     void Start()
     {
         CanvasObject = GetComponent<Canvas> ();
+        CanvasObject.enabled = false;
+        Audio = GetComponentInChildren<AudioSource>();
     }
  
     void Update() 
     {
-        if (Input.GetKeyUp(KeyCode.Escape)){
-           CanvasObject.enabled = !CanvasObject.enabled;
+        if (!audioPlay){
+            Audio.Pause();
         }
+
+        if (Input.GetKeyUp(KeyCode.E)){
+            audioPlay = !audioPlay;
+           CanvasObject.enabled = !CanvasObject.enabled;
+           Audio.Play();
+        }
+        
     }
 }
