788f13643dfe1eabfb7649d660a9bad51b6e1df3
Iara Ravagni <iravagni@itba.edu.ar>
2022-11-17 19:41:42 -0300

    texto

--- Assets/Scenes/Scripts/Showingtext.cs
+++ Assets/Scenes/Scripts/Showingtext.cs
@@ -8,38 +8,46 @@ public class Showingtext : MonoBehaviour
     private GameObject ControlObject;
 
     [SerializeField]
-    private GameObject TextPrefab;
+    private GameObject TextObject;
 
-    [SerializeField]
-    private float KillTime;
+    //[SerializeField]
+    //private GameObject TextPrefab;
+
+    //[SerializeField]
+    //private float KillTime;
 
     public float DistanceToView = 2f;
     public Transform PlayerObject;
 
+    GameObject newText;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject newText = Instantiate(TextPrefab, ControlObject.transform.position, Quaternion.Euler(0, 90, 0));
-        
+        //GameObject newText = Instantiate(TextPrefab, ControlObject.transform.position, Quaternion.Euler(0, 90, 0));
+
         if (Vector3.Distance(PlayerObject.position, ControlObject.transform.position) <= DistanceToView){
-            newText.SetActive(true);
-        }else
-        {
-            newText.SetActive(false);
+            TextObject.SetActive(true);
+        }else{        
+            TextObject.SetActive(false);
         }
         
-        Destroy(newText.gameObject, KillTime);
+        //Destroy(newText.gameObject, KillTime);
         //newText.GetComponent<SetText>().SetTextFunction("Presione la tecla E");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //GameObject newText = Instantiate(TextPrefab, ControlObject.transform.position, Quaternion.Euler(0, 90, 0));
         if ((Vector3.Distance(PlayerObject.position, ControlObject.transform.position) <= DistanceToView)){
-            GameObject newText = Instantiate(TextPrefab, ControlObject.transform.position, Quaternion.Euler(0, 90, 0));
-            newText.SetActive(true);
-            Destroy(newText.gameObject, KillTime);
+            //GameObject newText = Instantiate(TextPrefab, ControlObject.transform.position, Quaternion.Euler(0, 90, 0));
+            TextObject.SetActive(true);
+            //Destroy(newText.gameObject, KillTime);
             //newText.GetComponent<SetText>().SetTextFunction("Presione la tecla E");
+        }else{        
+            TextObject.SetActive(false);
         }
+        //Destroy(newText.gameObject, 1);
     }
 }
